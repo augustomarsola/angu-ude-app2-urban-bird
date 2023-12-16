f5ffee7430dbcdec1273f26837c2c9d26c319574
Augusto Marsola <augusto.marsola@live.com>
2023-12-15 22:59:04 -0300

    Created offersService and offersModel

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OffersService } from '../services/offers.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
+  providers: [OffersService],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  constructor(private _offersService: OffersService) {}
+  ngOnInit(): void {
+    console.log(this._offersService.getOffers());
+  }
+}
