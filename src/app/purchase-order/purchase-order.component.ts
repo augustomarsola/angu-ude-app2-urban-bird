4ae8c5193744a493e442503e75d128dc5ec8138d
Augusto Marsola <augusto.marsola@live.com>
2023-12-31 18:03:51 -0300

    Added reactive forms treatments

--- src/app/purchase-order/purchase-order.component.ts
+++ src/app/purchase-order/purchase-order.component.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-purchase-order',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './purchase-order.component.html',
 })
-export class PurchaseOrderComponent {}
+export class PurchaseOrderComponent {
+  private _fb = inject(FormBuilder);
+  public purchaseOrderForm = this._fb.group({
+    address: ['', Validators.required],
+    number: ['', Validators.required],
+    complement: [''],
+    paymentMethod: ['', Validators.required],
+  });
+
+  public get address() {
+    return this.purchaseOrderForm.get('address');
+  }
+
+  public get isAddressInvalid() {
+    return this.address?.invalid && this.address?.touched;
+  }
+
+  public get number() {
+    return this.purchaseOrderForm.get('number');
+  }
+
+  public get isNumberInvalid() {
+    return this.number?.invalid && this.number?.touched;
+  }
+
+  public get paymentMethod() {
+    return this.purchaseOrderForm.get('paymentMethod');
+  }
+
+  public get isPaymentMethodInvalid() {
+    return this.paymentMethod?.invalid && this.paymentMethod?.touched;
+  }
+
+  public onSubmit() {
+    this.purchaseOrderForm.markAllAsTouched();
+    console.log(this.purchaseOrderForm.value);
+  }
+}
